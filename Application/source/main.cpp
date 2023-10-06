3f80a3f05b23e0335ff39a96ce162ae9baa46925
xMihai01 <doloiu22@icloud.com>
2023-10-06 14:58:49 +0300

    Added VideoCamera class to capture frames from video camera

--- Application/source/main.cpp
+++ Application/source/main.cpp
@@ -1,6 +1,17 @@
+#include <Model/VideoCamera.h>
+
 #include <iostream>
 
 int main() {
-	std::cout << "Licenta";
+	
+	VideoCamera videoCamera;
+	videoCamera.OpenCamera();
+	while (videoCamera.IsCameraOpened()) {
+		cv::imshow("Frame", videoCamera.GetCurrentFrame());
+		if (cv::waitKey(30) == 27)
+			break;
+	}
+	videoCamera.StopCamera();
+	
 	return 0;
 }
